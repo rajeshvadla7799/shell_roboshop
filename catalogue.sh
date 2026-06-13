d99a7d8f134f31dc1ffd5c90a79f1be79ee29eac
rajeshvadla7799 <rajesh.vadla7799@gmail.com>
2026-06-13 15:50:57 +0530

    commiting the files

--- catalogue.sh
+++ catalogue.sh
@@ -1,45 +1,81 @@
 #!/bin/bash
 
 USERID=$(id -u)
-LOGS_FOLDER="/var/log/Shell_scripts"
-SCRIPT_NAME=$(basename "$0" .sh)
-LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-
+LOGS_FOLDER="/var/log/Shell_roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+B="\e[34m"
+M="\e[35m"
 N="\e[0m"
 
-mkdir -p $LOGS_FOLDER
-
 if [ $USERID -ne 0 ]; then
-    echo -e "$R Please run this script as root user $N" | tee -a $LOGS_FILE
+    echo "$R Please run this script as root user $N" | tee -a $LOGS_FILE
     exit 1
 fi
 
-VALIDATE() {
-    if [ $1 -ne 0 ]; then
-        echo -e "$R $2 ... FAILURE $N" | tee -a $LOGS_FILE
+mkdir -p $LOGS_FOLDER
+
+VALIDATE () {
+    if [ $? -ne 0 ]; then
+        echo -e "$R $2... Failure $N" | tee -a $LOGS_FILE
         exit 1
     else
-        echo -e "$G $2 ... SUCCESS $N" | tee -a $LOGS_FILE
+        echo -e "$G $2... Success $N" | tee -a $LOGS_FILE
     fi
 }
 
-apt module disable nodejs &>> $LOGS_FILE
-VALIDATE $? "Disabling Node.js Module"
+dnf module disable nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Disabling NodeJS default module"
 
-apt module enable nodejs &>> $LOGS_FILE
-VALIDATE $? "Enabling Node.js Module"
+dnf module enable nodejs:20 -y &>>$LOGS_FILE
+VALIDATE $? "Enabling NodeJS 20 module"
 
-apt install nodejs -y &>> $LOGS_FILE
-VALIDATE $? "Installing Node.js"
+dnf install nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Installing NodeJS"
 
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOGS_FILE
-VALIDATE $? "Adding Application User"
+id roboshop &>>$LOGS_FILE
+if [ $? -ne 0 ]; then
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+VALIDATE $? "Adding roboshop user"
+else
+    echo -e "$Y roboshop user already exists, skipping user creation $N"
+fi
 
-mkdir /app &>> $LOGS_FILE
-VALIDATE $? "Creating Application Directory"
+mkdir -p /app &>>$LOGS_FILE
+VALIDATE $? "Creating application directory"
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
-VALIDATE $? "Downloading Application Code"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOGS_FILE
+VALIDATE $? "Downloading catalogue code"
+
+cd /app &>>$LOGS_FILE
+VALIDATE $? "Changing directory to /app"
+
+unzip /tmp/catalogue.zip &>>$LOGS_FILE
+VALIDATE $? "Extracting catalogue code"
+
+cd /app &>>$LOGS_FILE
+VALIDATE $? "Changing directory to /app"
+
+npm install &>>$LOGS_FILE
+VALIDATE $? "Installing NodeJS dependencies"
+
+cp /app/systemd/catalogue.service /etc/systemd/system/catalogue.service &>>$LOGS_FILE
+VALIDATE $? "Copying systemd service file"
+
+systemctl daemon-reload &>>$LOGS_FILE
+VALIDATE $? "Reloading systemd"
+
+systemctl enable catalogue &>>$LOGS_FILE
+VALIDATE $? "Enabling catalogue service"
+
+systemctl start catalogue
+VALIDATE $? "Starting catalogue service"
+
+
+
+
+
+
+
